4c2cb9828246c90b10faefab97b0852233425c3d
reivax-boucoi <reivax.boucoi@laposte.net>
2020-08-17 11:58:44 +0200

    Add files via upload

--- firmware/Screen.h
+++ firmware/Screen.h
@@ -6,10 +6,17 @@
 #include <LiquidCrystal.h>
 extern LiquidCrystal lcd;
 
+class Screen;
+class Value;
+
 class Value {
   public:
-    Value(float* v, int x, int y, int digits);
-    Value(float* v, int x, int y, int digits, float min, float max, void (*c)(void));
+    Value(float* v, uint8_t x, uint8_t y, uint8_t digits);
+    Value(float* v, uint8_t x, uint8_t y, uint8_t digits, float min, float max);
+	
+	Value *nextValue;
+	Value *prevValue;
+	
     void up(void);
     void down(void);
     void advanceCursor(void);
@@ -21,33 +28,35 @@ class Value {
     float minVal;
     float maxVal;
     
-    int posX;
-    int posY;
-    int nbDigits;
-    int DPpos; //nb of significant digits
+    uint8_t posX;
+    uint8_t posY;
+    uint8_t nbDigits;
+    uint8_t DPpos; //nb of significant digits
     bool editable=false;
-    int cursorPos=0;
-    void (*callback)(void);
+    uint8_t cursorPos=0;
 };
 
 
 class Screen {
   public:
-    Screen(const char* line1, const char* line2, Value *value0, Value *value1, Value *value2, Value *value3);
+    Screen(Value *v);
+	void addValue(Value *v);
     void show(void);
     void refresh(void);
     void enter(void);
     void next(void);
     void prev(void);
     void back(void);
-    Value *values[4];
+	Screen* nextScreen;
+	Screen* prevScreen;
+	
     bool valueEditing=false;
+    const __FlashStringHelper* txt1;
+    const __FlashStringHelper* txt2;
     
     private:
-    void getNextValue(int dir);
-    const char* txt1;
-    const char* txt2;
-    int selectedValue=-1;
+    void getNextValue(bool dir);
+    Value *selectedValue;
 };
 
 
