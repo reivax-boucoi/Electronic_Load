be839130ce2538d3eeeade607fd82bd5661a92d4
reivax-boucoi <reivax.boucoi@laposte.net>
2020-08-17 11:58:24 +0200

    Add files via upload

--- firmware/Utils.cpp
+++ firmware/Utils.cpp
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-int i2c_write(byte deviceAddr, int address, long data, int l) {
+uint8_t i2c_write(byte deviceAddr, uint8_t address, long data, uint8_t l) {
   Wire.beginTransmission(deviceAddr);
   Wire.write(byte(address)); // address specifier
   while (l > 0) {
@@ -10,13 +10,13 @@ int i2c_write(byte deviceAddr, int address, long data, int l) {
   byte error = Wire.endTransmission();
   if (error != 0) {
     Serial.println(F("Error during write"));
-    return -1;
+    return 1;
   }
   delay(1);
   return 0;
 }
 
-long i2c_read(byte deviceAddr, int address, int l) {
+long i2c_read(byte deviceAddr, uint8_t address, uint8_t l) {
   Wire.beginTransmission(deviceAddr);
   Wire.write(byte(address));
   Wire.endTransmission(false);//repeated start !! mandatory !!
@@ -35,10 +35,10 @@ long i2c_read(byte deviceAddr, int address, int l) {
   }
   //Serial.println(" available");
   long res = 0;
-  for (int i = 0; i < l; i++) {
+  for (uint8_t i = 0; i < l; i++) {
  /* Serial.print(deviceAddr,HEX);
   Serial.print(" request");*/
-    int value = Wire.read();
+    uint8_t value = Wire.read();
  // Serial.println(" response");
     //if(address==REG_S1)Serial.println(value,HEX);
     res = (res << 8) + byte(value);
