ec4403900d61fb016072a78eea7aa60f0a876415
reivax-boucoi <reivax.boucoi@laposte.net>
2020-10-22 19:41:42 +0200

    added board define

--- firmware/Utils.h
+++ firmware/Utils.h
@@ -4,6 +4,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#define BOARD1  //First version, comment to use second board
 
 uint8_t i2c_write(byte deviceAddr,uint8_t address, long data, uint8_t l);
 long i2c_read(byte deviceAddr, uint8_t address, uint8_t l);
